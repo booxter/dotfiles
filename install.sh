d32689872654e3bd5ecdc87a40696e9561727506
Ihar Hrachyshka <ihrachys@redhat.com>
2023-10-17 02:10:33 +0000

    Simplify stowing

--- install.sh
+++ install.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
-
 set -e
-SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-PRESENT=false
-
 DOT_FOLDERS="bash,git,nvim,tmux,zsh"
 
-for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
-  echo "[+] Folder :: $folder"
-
-  stow --ignore=README.md --ignore=LICENSE \
-    -t $HOME -D $folder
-  stow -v -t $HOME $folder
-done
+stow --verbose -t $HOME --restow $(echo $DOT_FOLDERS | sed "s/,/ /g")
 
 # Reload shell once installed
 echo "[+] Reloading shell..."
