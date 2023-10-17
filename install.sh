2e36ca9d07fd8afa0893c371777b0d9f2a704d00
Ihar Hrachyshka <ihrachys@redhat.com>
2023-10-17 16:37:27 -0400

    Add iterm2 config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-DOT_FOLDERS="bash,git,nvim,tmux,zsh"
+DOT_FOLDERS="bash,git,iterm2,nvim,tmux,zsh"
 
 stow --verbose -t $HOME --restow $(echo $DOT_FOLDERS | sed "s/,/ /g")
 
