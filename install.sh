086894b56a7f66634d6f5088347194bdee2ea344
Ihar Hrachyshka <ihrachys@redhat.com>
2023-10-18 14:57:30 +0000

    Create an (invalid) git-pw token gitconfig

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ DOT_FOLDERS="bash,git,iterm2,nvim,tmux,zsh"
 
 stow --verbose -t $HOME --restow $(echo $DOT_FOLDERS | sed "s/,/ /g")
 
+if [ ! -e $HOME/.git-pw-token-config ]; then
+	cp git-pw-token-config $HOME/.git-pw-token-config
+	echo "Don't forget to configure git [pw] token!"
+fi
+
 # Reload shell once installed
 echo "[+] Reloading shell..."
 exec $SHELL -l
