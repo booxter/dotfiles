bcf59e91069f4aec5dda05f0133518296464da73
Ihar Hrachyshka <ihrachys@redhat.com>
2023-10-18 14:57:49 +0000

    Don't reload shell after install

--- install.sh
+++ install.sh
@@ -8,7 +8,3 @@ if [ ! -e $HOME/.git-pw-token-config ]; then
 	cp git-pw-token-config $HOME/.git-pw-token-config
 	echo "Don't forget to configure git [pw] token!"
 fi
-
-# Reload shell once installed
-echo "[+] Reloading shell..."
-exec $SHELL -l
