7fad630d940f51651e8910a691b50ea776d1409b
Ihar Hrachyshka <ihrachys@redhat.com>
2023-10-18 15:11:36 +0000

    Add todo to explcitly choose server version for pw

--- home_bin/bin/list-ovn-patches.sh
+++ home_bin/bin/list-ovn-patches.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# TODO: this prints errors like
+# You should provide the server version in the URL configured via git-config or --server
 git pw --project openvswitch patch list --state='changes requested' --submitter=ihrachys@redhat.com
