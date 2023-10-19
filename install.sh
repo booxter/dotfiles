b0266ae19d36dc56c9926c86207f8cc35e0fc1a1
Ihar Hrachyshka <ihrachys@redhat.com>
2023-10-19 16:53:32 +0000

    git: configure for send-email via work gmail

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ DOT_FOLDERS="bash,git,home_bin,iterm2,nvim,tmux,zsh"
 
 stow --verbose -t $HOME --restow $(echo $DOT_FOLDERS | sed "s/,/ /g")
 
-if [ ! -e $HOME/.git-pw-token-config ]; then
-	cp git-pw-token-config $HOME/.git-pw-token-config
-	echo "Don't forget to configure git [pw] token!"
+if [ ! -e $HOME/.gitconfig.local ]; then
+	cp gitconfig.local $HOME/.gitconfig.local
+	echo "Don't forget to configure .gitconfig.local"
 fi
