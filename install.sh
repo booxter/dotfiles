1ca857ccb6ff1710daf573816fdb291e1b3a0611
Ihar Hrachyshka <ihrachys@redhat.com>
2023-10-22 03:24:06 +0000

    Clean up bash_profile too

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 DOT_FOLDERS="bash,git,gri,home_bin,iterm2,nvim,tmux,zsh"
 
 # TODO: reconsider a better approach to clean up system default files?
-rm -f $HOME/.bashrc
+rm -f $HOME/.bashrc $HOME/.bash_profile
 
 stow --verbose -t $HOME --restow $(echo $DOT_FOLDERS | sed "s/,/ /g")
 
