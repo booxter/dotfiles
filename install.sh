601143677f96b1a7670bb9e2e933db0b73d32c5c
Ihar Hrachyshka <ihrachys@redhat.com>
2023-10-22 03:10:44 +0000

    Clean up .bashrc before stowing

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ set -e
 # TODO: introspect repo instead of listing each module here
 DOT_FOLDERS="bash,git,gri,home_bin,iterm2,nvim,tmux,zsh"
 
+# TODO: reconsider a better approach to clean up system default files?
+rm $HOME/.bashrc
+
 stow --verbose -t $HOME --restow $(echo $DOT_FOLDERS | sed "s/,/ /g")
 
 if [ ! -e $HOME/.gitconfig.local ]; then
