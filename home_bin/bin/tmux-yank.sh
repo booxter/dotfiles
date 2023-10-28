928ccdbdd5722c5e44fcea3fef71b4ad4ca1ca71
Ihar Hrachyshka <ihrachys@redhat.com>
2023-10-28 17:22:37 +0000

    Add script to yank stdin into tmux buffer (and clipboard)

---
+++ home_bin/bin/tmux-yank.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Find a filename to use as a temp file
+#
+# Stolen from https://blog.landofcrispy.com/index.php/2021/01/06/clipboard-integration-between-tmux-nvim-zsh-x11-across-ssh-sessions/
+
+TEMPFILE="`tempfile 2>/dev/null`"
+if [ $? -ne 0 ]; then
+	TEMPFILE="/tmp/_clip_temp_yssh$USER"
+fi
+
+# Save stdin to file
+cat > "$TEMPFILE"
+
+# Load tmux buffer
+tmux load-buffer -w "$TEMPFILE"
+
+# Remove file
+rm -f "$TEMPFILE"
