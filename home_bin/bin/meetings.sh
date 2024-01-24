6cfc20393e801a8f222560be8ad285c47337410e
Ihar Hrachyshka <ihrachys@redhat.com>
2024-01-23 21:46:34 -0500

    meetings.sh: remove tabs

--- home_bin/bin/meetings.sh
+++ home_bin/bin/meetings.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 if [ "x" = "x${DATE}" ]; then
-	if [ ! $# -eq 1 ]; then
-	  DATE=$(date "+%Y-%m-%d")
-	else
-		DATE=$1
-	fi
+  if [ ! $# -eq 1 ]; then
+    DATE=$(date "+%Y-%m-%d")
+  else
+    DATE=$1
+  fi
 fi
 
 ~/bin/icalBuddy \
-	-b "- " -nnr " " -eed -npn -nc -ps "/ --- /" \
-	-iep "title,notes" -po "title,notes" \
-	-df "%Y-%M-%D" \
-	eventsFrom:$DATE to:$DATE |\
+  -b "- " -nnr " " -eed -npn -nc -ps "/ --- /" \
+  -iep "title,notes" -po "title,notes" \
+  -df "%Y-%M-%D" \
+  eventsFrom:$DATE to:$DATE |\
 \
   grep meet.google.com | grep reclaim.ai |\
-	sed "s/ ---.*$//g" |\
-  sed "s/^-/$DATE/g" |\
-	sed "s/[\/|]/~/g" |\
-	sed "s/:/-/g"
+    sed "s/ ---.*$//g" |\
+    sed "s/^-/$DATE/g" |\
+    sed "s/[\/|]/~/g" |\
+    sed "s/:/-/g"
