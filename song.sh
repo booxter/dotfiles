54d77316290ab650447248a3754eb64a7b233a3d
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-01-09 22:07:06 -0500

    Add script to print out current song from spotify_player

---
+++ song.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+response=$(spotify_player get key playback)
+
+artist_names=$(echo "$response" | jq -r '.item.artists[].name' | paste -sd ", ")
+track_name=$(echo "$response" | jq -r '.item.name')
+
+echo "$artist_names - $track_name"
