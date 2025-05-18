d3387cdd3ed322ed666751268e5895675d6137a4
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-05-18 18:32:48 -0400

    rewrap htop-all.sh
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- htop-all.sh
+++ htop-all.sh
@@ -3,25 +3,25 @@
 # This script will start or attach a new tmux session with four panes, each running htop per builder.
 
 HOSTS=(
-		"darwin-builder"
-		"linux-builder"
-		"linux-x86-builder"
-		"localhost"
+  "darwin-builder"
+  "linux-builder"
+  "linux-x86-builder"
+  "localhost"
 )
 
 # Check if tmux is installed
 if ! command -v tmux &> /dev/null
 then
-		echo "tmux could not be found. Please install it first."
-		exit
+  echo "tmux could not be found. Please install it first."
+  exit
 fi
 
 # Check if a tmux session is already running
 #
 if tmux list-sessions 2>/dev/null | grep -q "htop"; then
-		echo "A tmux session named 'htop' is already running. Attaching to it..."
-		tmux attach-session -t htop
-		exit 0
+  echo "A tmux session named 'htop' is already running. Attaching to it..."
+  tmux attach-session -t htop
+  exit 0
 fi
 
 # Create a new tmux session
@@ -35,13 +35,13 @@ tmux split-window -v
 
 # Send htop command to each pane
 for i in "${!HOSTS[@]}"; do
-		host="${HOSTS[$i]}"
-		tmux select-pane -t $((i+1))
-		if [[ $host == "localhost" ]]; then
-				tmux send-keys "htop; exit" C-m
-		else
-				tmux send-keys "ssh -t $host 'htop'; exit" C-m
-		fi
+  host="${HOSTS[$i]}"
+  tmux select-pane -t $((i+1))
+  if [[ $host == "localhost" ]]; then
+    tmux send-keys "htop; exit" C-m
+  else
+    tmux send-keys "ssh -t $host 'htop'; exit" C-m
+  fi
 done
 
 # Select the first pane
